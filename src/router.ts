443dda90f29999bbf4aa55fba2b74fc479fec41f
Airyzz <36567925+Airyzz@users.noreply.github.com>
2023-11-16 16:22:56 +1030

    Fixing responses

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,15 @@ const router = Router();
 
 router.post('/_matrix/push/v1/notify', async (request: Request, env: Env) => {
     await notify.notify(request, env);
+    return new Response("{}", {
+        status: 200, headers: {
+            "content-type": "application/json"
+        }
+    })
+
+});
+
+router.get('/health', async (request: Request, env: Env) => {
     return new Response("", { status: 200 })
 });
 
